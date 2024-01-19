873abb6bde00bf8edeaa8e620a8b9743db5fd752
19870526zxp <157121845+19870526zxp@users.noreply.github.com>
2024-01-19 22:28:03 +0800

    Add files via upload

--- FirstGitHubClass.java
+++ FirstGitHubClass.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println("Hello,GitHub!");
+        System.out.println("Hello,GitHub!Again!");
     }
 }
